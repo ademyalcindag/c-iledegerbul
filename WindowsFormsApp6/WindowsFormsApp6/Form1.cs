3a0fed651bec708cc8e1705d13789e05c312614e
ademyalcindag <ademyalcindag2@gmail.com>
2022-05-18 10:08:48 +0300

    c#iledegerbul
    birincideger ikincideğeri bul arat bul

---
+++ WindowsFormsApp6/WindowsFormsApp6/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int sayi = 0;
+            int sayi1= 1;
+            int tplm;
+            int grln;
+
+
+            grln = Convert.ToInt32(textBox1.Text);
+            fibonacci.Items.Add(sayi1);
+            for (int a = 0; a < grln - a; a++)
+            {
+                tplm= sayi + sayi1;
+                fibonacci.Items.Add(tplm);
+                sayi = sayi1;
+                sayi1 = tplm;
+            }
+
+        }
+
+        private void fibonacci_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
